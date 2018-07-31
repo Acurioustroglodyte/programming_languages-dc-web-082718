4a5c550112765323758c1bd5f965fd660cb64287
Trevor Jameson <tjameson15@gmail.com>
2018-07-31 00:44:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,53 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |lang_style, style_data|
+    style_data.each do |lang_name, name_data|
+      if new_hash[lang_name] == true
+        new_hash[lang_name][:style] << lang_style
+      else
+        new_hash[lang_name] = {}
+        new_hash[lang_name][:type] = name_data[:type]
+        new_hash[lang_name][:style] = [lang_style]
+        binding.pry
+      end
+      end
+    end
+  end
+  new_hash
 end
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+reformat_languages(languages)
